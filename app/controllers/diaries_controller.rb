860948a59e58453815fe63f91f2028f662088fe7
tysmmsy <68370113+tysmmsy@users.noreply.github.com>
2020-08-22 17:09:07 +0900

    日記表示機能の実装(ビュー調整)

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -1,5 +1,7 @@
 class DiariesController < ApplicationController
   def index
+    @diaries = Diary.all.order(id:"DESC")
+    @diaries = @diaries.includes(:user)
   end
 
   def create
@@ -8,6 +10,7 @@ class DiariesController < ApplicationController
     if @diary.save
       redirect_to user_diaries_path(@user)
     else
+      @diaries = @diaries.includes(:user)
       render :index
     end
   end
