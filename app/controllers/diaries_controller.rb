267e87e7f58125e8d8bfe235784a66737985d50f
tysmmsy <68370113+tysmmsy@users.noreply.github.com>
2020-08-22 11:54:21 +0900

    日記投稿機能の実装

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -1,4 +1,20 @@
 class DiariesController < ApplicationController
   def index
   end
+
+  def create
+    @user = User.find(params[:user_id])
+    @diary = @user.diaries.new(diary_params)
+    if @diary.save
+      redirect_to user_diaries_path(@user)
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def diary_params
+    params.permit(:content).merge(user_id: current_user.id)
+  end
 end
