6f7e996468a8486b10819d339cb70d0f6f9fc1b8
tysmmsy <68370113+tysmmsy@users.noreply.github.com>
2020-09-03 18:22:52 +0900

    devise関連のビューファイルのCSSを変更

--- app/javascript/effect.js
+++ app/javascript/effect.js
@@ -1,10 +1,6 @@
 window.onload = function() {
   scroll_effect();
 
-  $(window).scroll(function(){
-    scroll_effect();
-  });
-
   function scroll_effect(){
     $('.effect-fade').each(function(){
       var elemPos = $(this).offset().top;
